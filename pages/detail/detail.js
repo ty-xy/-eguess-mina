1fad0a8aacd476de244d8f87ae5f71e817b8d176
toringo <zg-gwt@zg18.com>
2018-03-15 14:38:58 +0800

    feat: 点赞，收藏

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -3,21 +3,11 @@ const app = getApp();
 const size = 5;
 let page = 1;
 const loadMore = function(that, option){
-    // API.ajax('/answer', { topicid: option.id,  }, function (res) {
-    //     console.log('answer', res)
-    //     //这里既可以获取模拟的res
-    //     if (res.statusCode === 200) {
-    //         that.setData({
-    //             list: res.data,
-    //             userid: app.globalData.userid,
-    //         })
-    //     }
-    // });
     that.setData({
         isLoading: true,
         noMore: false,
     })
-    API.ajax('/answer', { limit: page * size, topicid: option.id, userid: '5aa369c76b05d06032f381c5' }, function (res) {
+    API.ajax('/answer', { limit: page * size, topicid: option.id, userid: '5a9a46834aff491a7530becd' }, function (res) {
         //这里既可以获取模拟的res
         if (res.statusCode === 200) {
             if (res.data.length === that.data.list.length) {
@@ -74,78 +64,71 @@ Page({
     // 点赞
     handlelike(e) {
         const { message } = e.currentTarget.dataset;
-        const userid = app.globalData.userid;
+        const userid = app.globalData.userid || '5a9a46834aff491a7530becd';
         const that = this;
-        
         const { option } = this.data;
-        const likes = message.like;
-        let method = 'post';
-        let likeId = '';
-        for (let i = 0; i < likes.length; i++) {
-            if (message.id === likes[i].answerid) {
-                method = 'delete';
-                likeId = likes[i].id;
-                break;
-            }
-        }
-        if (method === 'post') {
-            API.ajax('/like', JSON.stringify({ userid: '5a9a46834aff491a7530becd', answerid: message.id, user: '5a9a46834aff491a7530becd', answer: message.id }), function (res) {
-                if (res.statusCode == 200 || res.statusCode == 201) {
-                    loadMore(that, option)
-                    that.show('点赞成功', 'dianzan1', '#FFD62D');
-                } else {
-                    that.show('点赞失败', 'cuowu', '#F45353');
+        const { upVotes, upVote } = message;
+        const resData = [];
+        if (upVote) {
+            for (let i = 0; i < upVotes.length; i++) {
+                if (userid !== upVotes[i].id) {
+                    resData.push(upVotes[i].id);
                 }
-            }, method);
+            }
         } else {
-            API.ajax(`/like/${likeId}`, '', function (res) {
-                console.log('res', res)
-                if (res.statusCode === 200) {
-                    loadMore(that, option)
+            for (let i = 0; i < upVotes.length; i++) {
+                resData.push(upVotes[i].id);
+            }
+            resData.push(userid);
+        }
+        API.ajax(`/answer/${message.id}`, JSON.stringify({ upVotes: resData }), function (res) {
+            if (res.statusCode == 200 || res.statusCode == 201) {
+                loadMore(that, option)
+                if (upVote) {
                     that.show('点赞已取消', 'dianzan', '#666');
                 } else {
-                    that.show('操作失败', 'cuowu', '#F45353');
+                    that.show('点赞成功', 'dianzan1', '#FFD62D');
                 }
-            }, method);
-        }
+                
+            } else {
+                that.show('点赞失败', 'cuowu', '#F45353');
+            }
+        }, 'put');
         
     },
     // 收藏
     handleMark(e) {
         const { message } = e.currentTarget.dataset;
-        const userid = app.globalData.userid;
+        const userid = app.globalData.userid || '5a9a46834aff491a7530becd';
         const that = this;
         const { option } = this.data;
-        const likes = message.like;
-        let method = 'post';
-        let likeId = '';
-        for (let i = 0; i < likes.length; i++) {
-            if (message.id === likes[i].answerid) {
-                method = 'delete';
-                likeId = likes[i].id;
-                break;
+        const { stars, isStar } = message;
+        const resData = [];
+        if (isStar) {
+            for (let i = 0; i < stars.length; i++) {
+                if (userid !== stars[i].id) {
+                    resData.push(stars[i].id);
+                }
             }
-        }
-        if (method === 'post') {
-            API.ajax('/like', JSON.stringify({ userid: '5a9a46834aff491a7530becd', answerid: message.id, user: '5a9a46834aff491a7530becd', answer: message.id }), function (res) {
-                if (res.statusCode == 200 || res.statusCode == 201) {
-                    loadMore(that, option)
-                    that.show('点赞成功', 'dianzan1', '#FFD62D');
-                } else {
-                    that.show('点赞失败', 'cuowu', '#F45353');
-                }
-            }, method);
         } else {
-            API.ajax(`/like/${likeId}`, '', function (res) {
-                console.log('res', res)
-                if (res.statusCode === 200) {
-                    loadMore(that, option)
-                    that.show('点赞已取消', 'dianzan', '#666');
-                } else {
-                    that.show('操作失败', 'cuowu', '#F45353');
-                }
-            }, method);
+            for (let i = 0; i < stars.length; i++) {
+                resData.push(stars[i].id);
+            }
+            resData.push(userid);
         }
+        API.ajax(`/answer/${message.id}`, JSON.stringify({ stars: resData }), function (res) {
+            if (res.statusCode == 200 || res.statusCode == 201) {
+                loadMore(that, option)
+                if (isStar) {
+                    that.show('收藏已取消', 'shoucang', '#666');
+                } else {
+                    that.show('收藏成功', 'shoucang1', '#FFD62D');
+                }
+                
+            } else {
+                that.show('收藏失败', 'cuowu', '#F45353');
+            }
+        }, 'put');
     },
     //页面滑动到底部
     bindDownLoad() {   
